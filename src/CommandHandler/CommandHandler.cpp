3f43165a18c1bb02ce346b83f43d3c5473ce60a7
Fangjun Zhou <fzhou48@wisc.edu>
2022-05-03 16:35:56 -0500

    Update: CommandHandler Added SplitCommand function.

--- src/CommandHandler/CommandHandler.cpp
+++ src/CommandHandler/CommandHandler.cpp
@@ -1,6 +1,6 @@
 #include "CommandHandler.hpp"
 
-int CommandHandler::HandleCommand(std::string &command)
+int CommandHandler::HandleCommand(const std::string &command)
 {
     // Exit the process
     if (command == "exit")
@@ -9,4 +9,47 @@ int CommandHandler::HandleCommand(std::string &command)
     }
 
     return 0;
+}
+
+std::vector<std::string> &CommandHandler::SplitCommand(std::string command)
+{
+    std::vector<std::string> *commandList = new std::vector<std::string>();
+
+    // All the available delimiters.
+    std::vector<std::string> delimiters = {" ", "\t"};
+    // The current delimiter.
+    std::string delimiter;
+    // The start location of that delimiter.
+    size_t delimiterLoc;
+    // The splitted token;
+    std::string token;
+
+    // Split base on delimiters.
+    while (true)
+    {
+        // Find the smallest delimiter and its position.
+        delimiterLoc = std::string::npos;
+        for (int i = 0; i < delimiters.size(); i++)
+        {
+            size_t newVal = command.find(delimiters[i]);
+            if (newVal < delimiterLoc)
+            {
+                delimiter = delimiters[i];
+                delimiterLoc = newVal;
+            }
+        }
+
+        if (delimiterLoc == std::string::npos)
+            break;
+
+        token = command.substr(0, delimiterLoc);
+        command.erase(0, delimiterLoc + delimiter.length());
+
+        if (token.length() != 0)
+        {
+            commandList->push_back(token);
+        }
+    }
+
+    return *commandList;
 }
--- src/CommandHandler/CommandHandler.hpp
+++ src/CommandHandler/CommandHandler.hpp
@@ -1,6 +1,8 @@
 #ifndef __COMMANDHANDLER_H__
 #define __COMMANDHANDLER_H__
 #include <string>
+#include <vector>
+#include <algorithm>
 
 namespace CommandHandler
 {
@@ -10,7 +12,16 @@ namespace CommandHandler
      * @param command the string command pass to the handler.
      * @return int -1 if fail, 0 if success.
      */
-    int HandleCommand(std::string &command);
+    int HandleCommand(const std::string &command);
+
+    /**
+     * @brief Slip the command based on space and tabs.
+     * Allocating a new vector of strings on heap and callers are responsable to deallocate them.
+     *
+     * @param command the string command copy.
+     * @return std::vector<std::string> & an vector of strings.
+     */
+    std::vector<std::string> &SplitCommand(std::string command);
 }
 
 #endif // __COMMANDHANDLER_H__
