114ec1e5edbbac145fef3069d33a2aa287039969
Fangjun Zhou <fzhou48@wisc.edu>
2022-05-03 02:05:03 -0500

    Update: main.cpp Fork when executing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <fstream>
+
 #include <string>
+
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
 #include "config.h"
+#include "CommandHandler/CommandHandler.hpp"
 
 int main(int, char **)
 {
-    // Debug purpose: Close cout and open a file.
-    std::ofstream redirFile;
-    int stdoutFileNo = dup(STDOUT_FILENO);
-
-    close(STDOUT_FILENO);
-    redirFile.open("redir.txt");
-
+    // Print version.
     std::cout << PROJECT_NAME << " v" << PROJECT_VER << std::endl;
+
     std::string command;
-
-    redirFile.close();
-
-    dup2(stdoutFileNo, STDOUT_FILENO);
 
     // Main loop.
     while (true)
@@ -26,5 +23,26 @@ int main(int, char **)
         std::cout << "Dong Shell>";
         std::cin >> command;
         std::cout << "Command: " << command << std::endl;
+        int res = CommandHandler::HandleCommand(command);
+        if (res == -1)
+        {
+            std::cerr << "CommandHandler error\n";
+            exit(1);
+        }
+
+        // Fork a new process and execute the target execuatble.
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            // Child Process
+            std::cout << "Child process executing: " << command << std::endl;
+
+            // TODO: Execute here.
+
+            exit(0);
+        }
+        // Wait for child process finished.
+        waitpid(pid, NULL, 0);
+        std::cout << "Forked child process " << pid << std::endl;
     }
 }
