7648e6425fd9a89b52189683e669df6d3f33f13f
Fangjun Zhou <fzhou48@wisc.edu>
2022-05-03 01:15:26 -0500

    Remove: build dir

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/fangjunzhou/Documents/CPP_Project/Dong-Shell
-# Build directory: /Users/fangjunzhou/Documents/CPP_Project/Dong-Shell/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
