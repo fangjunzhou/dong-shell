866be4dd2071715fad7f57dd7c5c3516372d5d42
Fangjun Zhou <fzhou48@wisc.edu>
2022-05-03 16:36:40 -0500

    Update: main.cpp Use getline to read the whole line from cin. Use SplitCommand.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 
 #include <string>
+#include <vector>
 
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -20,8 +21,9 @@ int main(int, char **)
     // Main loop.
     while (true)
     {
+        command.clear();
         std::cout << "Dong Shell>";
-        std::cin >> command;
+        std::getline(std::cin, command);
         std::cout << "Command: " << command << std::endl;
         int res = CommandHandler::HandleCommand(command);
         if (res == -1)
@@ -30,6 +32,9 @@ int main(int, char **)
             exit(1);
         }
 
+        // Split the command.
+        std::vector<std::string> &commandList = CommandHandler::SplitCommand(command);
+
         // Fork a new process and execute the target execuatble.
         pid_t pid = fork();
         if (pid == 0)
@@ -37,12 +42,27 @@ int main(int, char **)
             // Child Process
             std::cout << "Child process executing: " << command << std::endl;
 
-            // TODO: Execute here.
+            // TODO: Remove this debug print.
+            std::cout << "commandList: \n";
+            for (int i = 0; i < commandList.size(); i++)
+            {
+                std::cout << commandList[i];
+                if (i != commandList.size() - 1)
+                {
+                    std::cout << ", ";
+                }
+            }
+            std::cout << std::endl;
+
+            // TODO: Construct the arguments and path.
 
             exit(0);
         }
         // Wait for child process finished.
         waitpid(pid, NULL, 0);
         std::cout << "Forked child process " << pid << std::endl;
+
+        // Deallocating the vector
+        delete &commandList;
     }
 }
