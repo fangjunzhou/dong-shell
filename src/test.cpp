74803e9d86a93c96af757f334bb1fabce12da1ef
Fangjun Zhou <fzhou48@wisc.edu>
2022-05-07 21:49:52 -0500

    Update: test.cpp Added constructor and destructor test.

--- src/test.cpp
+++ src/test.cpp
@@ -3,5 +3,37 @@
 
 TEST_CASE("DisplayBuffer Constructor and Destructor", "[DisplayBuffer]")
 {
-    DisplayBuffer<int> buffer = DisplayBuffer<int>({10, 10});
+    SECTION("Stack Allocated DisplayBuffer")
+    {
+        DisplayBuffer<int> buffer = DisplayBuffer<int>({2, 4});
+
+        BufferSize size = buffer.GetBufferSize();
+        REQUIRE(size.width == 2);
+        REQUIRE(size.height == 4);
+    }
+
+    SECTION("Heap Allocated DisplayBuffer")
+    {
+        DisplayBuffer<int> *buffer = new DisplayBuffer<int>({2, 4});
+
+        BufferSize size = buffer->GetBufferSize();
+        REQUIRE(size.width == 2);
+        REQUIRE(size.height == 4);
+
+        delete buffer;
+        SUCCEED("Deallocated buffer obj.");
+    }
+
+    SECTION("Constructor Invalid Arguments")
+    {
+        try
+        {
+            DisplayBuffer<int> buffer = DisplayBuffer<int>({-1, 4});
+            FAIL("std::invalid_argument not thrown.");
+        }
+        catch (const std::invalid_argument &IAE)
+        {
+            SUCCEED("Successfully throw exception.");
+        }
+    }
 }
