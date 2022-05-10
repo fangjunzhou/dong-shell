9c153532e696d7f3917936d0983ec6a0912268fc
Fangjun Zhou <fzhou48@wisc.edu>
2022-05-09 21:45:49 -0500

    Update: VerticalDisplay Added GetCharBuffer and GetFlushBuffer method to fetch buffer directly. Added Flush to flush content to display. Added UpdateChar to use an easy way tp update char buffer and bitmap. Added ClearDisplay to clear the entire console.

--- src/ShellDisplay/VerticalDisplay.cpp
+++ src/ShellDisplay/VerticalDisplay.cpp
@@ -1,23 +1,34 @@
 #include "VerticalDisplay.hpp"
 
+#define CLEAR_SCR "\033[2J"
+#define MOVE_CURSOR(x, y) ("\033[" + std::to_string(y) + ";" + std::to_string(x) + "H")
+
 #pragma region Constructor and Destructor
 
 VerticalDisplay::VerticalDisplay(eventpp::CallbackList<void(int width, int height)> *consoleResizeCallback, BufferSize size)
 {
     // Register callback
     m_consoleResizeEvent = consoleResizeCallback;
-    m_consoleResizeHandle = m_consoleResizeEvent->append([this](int width, int height)
-                                                         { OnConsoleResize(width, height); });
+    if (m_consoleResizeEvent != NULL)
+    {
+        m_consoleResizeHandle = m_consoleResizeEvent->append([this](int width, int height)
+                                                             { OnConsoleResize(width, height); });
+    }
+
+    m_consoleSize = size;
 
     // Init DisplayBuffer
-    m_charBuffer = new DisplayBuffer<char>(size);
-    m_flushBitmap = new DisplayBuffer<bool>(size);
+    m_charBuffer = new DisplayBuffer<char>(size, '\0');
+    m_flushBitmap = new DisplayBuffer<bool>(size, true);
 }
 
 VerticalDisplay::~VerticalDisplay()
 {
     // Unregister callback;
-    m_consoleResizeEvent->remove(m_consoleResizeHandle);
+    if (m_consoleResizeEvent != NULL)
+    {
+        m_consoleResizeEvent->remove(m_consoleResizeHandle);
+    }
 
     // Deallocate DisplayBuffer
     delete m_charBuffer;
@@ -26,6 +37,57 @@ VerticalDisplay::~VerticalDisplay()
 
 #pragma endregion
 
+#pragma region Private Field
+
 void VerticalDisplay::OnConsoleResize(int width, int height)
 {
+    // Reset the console size.
+    m_consoleSize.width = width;
+    m_consoleSize.height = height;
 }
+
+#pragma endregion
+
+#pragma region Public Field
+
+void VerticalDisplay::ClearDisplay()
+{
+    std::cout << CLEAR_SCR;
+}
+
+void VerticalDisplay::Flush()
+{
+    // Flush all the content in the buffer onto the screen.
+    for (int y = 0; y < m_consoleSize.height; y++)
+    {
+        for (int x = 0; x < m_consoleSize.width; x++)
+        {
+            // Check bitmap.
+            if (!m_flushBitmap->GetBuffer()[x][y])
+                continue;
+            // Move cursor.
+            std::cout << MOVE_CURSOR(x, y);
+            std::cout << m_charBuffer->GetBuffer()[x][y];
+            // Make bitmap cold.
+            m_flushBitmap->SetBuffer(x, y, false);
+        }
+    }
+}
+
+void VerticalDisplay::UpdateChar(int x, int y, char val)
+{
+    m_charBuffer->SetBuffer(x, y, val);
+    m_flushBitmap->SetBuffer(x, y, true);
+}
+
+DisplayBuffer<char> &VerticalDisplay::GetCharBuffer()
+{
+    return *m_charBuffer;
+}
+
+DisplayBuffer<bool> &VerticalDisplay::GetFlushBitmap()
+{
+    return *m_flushBitmap;
+}
+
+#pragma endregion
--- src/ShellDisplay/VerticalDisplay.hpp
+++ src/ShellDisplay/VerticalDisplay.hpp
@@ -1,6 +1,7 @@
 #ifndef __VERTICALDISPLAY_H__
 #define __VERTICALDISPLAY_H__
 #include <iostream>
+#include <string>
 
 #include "DisplayBuffer.hpp"
 #include "eventpp/callbacklist.h"
@@ -10,6 +11,12 @@ using ResizeCL = eventpp::CallbackList<void(int width, int height)>;
 class VerticalDisplay
 {
 private:
+    /**
+     * @brief The current size of console.
+     *
+     */
+    BufferSize m_consoleSize;
+
     /**
      * @brief The buffer that stores all the character;
      *
@@ -46,6 +53,41 @@ private:
 public:
     VerticalDisplay(eventpp::CallbackList<void(int width, int height)> *consoleResizeCallback, BufferSize size);
     ~VerticalDisplay();
+
+    /**
+     * @brief Clear the entire display.
+     *
+     */
+    void ClearDisplay();
+
+    /**
+     * @brief Flush the m_charBuffer onto the screen.
+     *
+     */
+    void Flush();
+
+    /**
+     * @brief Update the char for a coordinate.
+     *
+     * @param x x coord.
+     * @param y y coord.
+     * @param val the char value.
+     */
+    void UpdateChar(int x, int y, char val);
+
+    /**
+     * @brief Get the Char Buffer directly.
+     *
+     * @return DisplayBuffer<char>&
+     */
+    DisplayBuffer<char> &GetCharBuffer();
+
+    /**
+     * @brief Get the Flush Bitmap directly.
+     *
+     * @return DisplayBuffer<bool>&
+     */
+    DisplayBuffer<bool> &GetFlushBitmap();
 };
 
 #endif // __VERTICALDISPLAY_H__
