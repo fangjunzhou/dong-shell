a0da184e70ecdf9c8e5ae4aee68d0810bd57c972
Fangjun Zhou <fzhou48@wisc.edu>
2022-05-10 12:20:50 -0500

    Update: VerticalDisplay Added PushString method. Change ClearDisplayLogic to only update the content in the display size.

--- src/ShellDisplay/VerticalDisplay.cpp
+++ src/ShellDisplay/VerticalDisplay.cpp
@@ -18,8 +18,22 @@ VerticalDisplay::VerticalDisplay(eventpp::CallbackList<void(int width, int heigh
     m_consoleSize = size;
 
     // Init DisplayBuffer
-    m_charBuffer = new DisplayBuffer<char>(size, '\0');
+    m_charBuffer = new DisplayBuffer<char>(size, ' ');
     m_flushBitmap = new DisplayBuffer<bool>(size, true);
+
+    // Set the virtual cursor to (width - 1, 0)
+    m_virtualCursorPos = {size.width - 1, 0};
+
+    // Set history buffer head.
+    m_bufferHead = m_historyBuffer;
+    m_bufferTail = m_historyBuffer;
+
+    // Clear the history buffer.
+    for (int i = 0; i < k_historyBufferSize; i++)
+    {
+        m_historyBuffer[i] = '\0';
+    }
+    m_historyLines = 0;
 }
 
 VerticalDisplay::~VerticalDisplay()
@@ -52,7 +66,15 @@ void VerticalDisplay::OnConsoleResize(int width, int height)
 
 void VerticalDisplay::ClearDisplay()
 {
-    std::cout << CLEAR_SCR;
+    // Clear all the buffer to initial state.
+    for (int y = 0; y < m_consoleSize.height; y++)
+    {
+        for (int x = 0; x < m_consoleSize.width; x++)
+        {
+            UpdateChar(x, y, ' ');
+        }
+    }
+    Flush();
 }
 
 void VerticalDisplay::Flush()
@@ -66,7 +88,7 @@ void VerticalDisplay::Flush()
             if (!m_flushBitmap->GetBuffer()[x][y])
                 continue;
             // Move cursor.
-            std::cout << MOVE_CURSOR(x, y);
+            std::cout << MOVE_CURSOR(x, y + 1);
             std::cout << m_charBuffer->GetBuffer()[x][y];
             // Make bitmap cold.
             m_flushBitmap->SetBuffer(x, y, false);
@@ -78,6 +100,95 @@ void VerticalDisplay::UpdateChar(int x, int y, char val)
 {
     m_charBuffer->SetBuffer(x, y, val);
     m_flushBitmap->SetBuffer(x, y, true);
+}
+
+void VerticalDisplay::PushString(std::string str)
+{
+    // The number of char in current line.
+    int lineCharCount = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        // Count history lines.
+        if (*m_bufferTail == '\n')
+            m_historyLines--;
+        // Start a new line.
+        if (str[i] == '\n' || lineCharCount == m_consoleSize.height)
+        {
+            m_historyLines++;
+            m_displayLines++;
+            lineCharCount = 0;
+        }
+        // Push the string to the circular buffer and increase the pointer.
+        *m_bufferTail = str[i];
+        m_bufferTail = m_historyBuffer + (m_bufferTail - m_historyBuffer + 1) % k_historyBufferSize;
+
+        lineCharCount++;
+
+        // Push the buffer head.
+        if (m_bufferTail == m_bufferHead)
+            m_bufferHead++;
+        if (m_bufferTail == m_displayHead)
+            m_displayHead++;
+    }
+
+    // TODO: Append new chars to the buffer to optimize performance.
+    if (m_displayLines <= m_consoleSize.width)
+    {
+        lineCharCount = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+            // Start a new line.
+            if (str[i] == '\n' || lineCharCount == m_consoleSize.height)
+            {
+                m_virtualCursorPos.x--;
+                m_virtualCursorPos.y = 0;
+                lineCharCount = 0;
+            }
+            if (str[i] == '\n')
+                continue;
+            // Update the char.
+            UpdateChar(m_virtualCursorPos.x, m_virtualCursorPos.y, str[i]);
+            m_virtualCursorPos.y++;
+            lineCharCount++;
+        }
+        return;
+    }
+
+    // The max available display lines should = console width.
+    lineCharCount = 1;
+    while (m_displayLines > m_consoleSize.width)
+    {
+        while (*m_displayHead != '\n' && lineCharCount != m_consoleSize.height)
+        {
+            m_displayHead = m_historyBuffer + (m_displayHead - m_historyBuffer + 1) % k_historyBufferSize;
+            lineCharCount++;
+        }
+        m_displayHead = m_historyBuffer + (m_displayHead - m_historyBuffer + 1) % k_historyBufferSize;
+        m_displayLines--;
+        lineCharCount = 1;
+    }
+
+    // TODO: Refresh the entire buffer.
+    ClearDisplay();
+    m_virtualCursorPos = {m_consoleSize.width - 1, 0};
+
+    lineCharCount = 0;
+    // Traverse through displayHead to bufferTail.
+    for (char *ptr = m_displayHead; ptr != m_bufferTail; ptr = m_historyBuffer + (ptr - m_historyBuffer + 1) % k_historyBufferSize)
+    {
+        // Start a new line.
+        if (*ptr == '\n' || lineCharCount == m_consoleSize.height)
+        {
+            m_virtualCursorPos.x--;
+            m_virtualCursorPos.y = 0;
+            lineCharCount = 0;
+            continue;
+        }
+        // Update the char.
+        UpdateChar(m_virtualCursorPos.x, m_virtualCursorPos.y, *ptr);
+        m_virtualCursorPos.y++;
+        lineCharCount++;
+    }
 }
 
 DisplayBuffer<char> &VerticalDisplay::GetCharBuffer()
--- src/ShellDisplay/VerticalDisplay.hpp
+++ src/ShellDisplay/VerticalDisplay.hpp
@@ -6,6 +6,14 @@
 #include "DisplayBuffer.hpp"
 #include "eventpp/callbacklist.h"
 
+struct CursorPos
+{
+    int x;
+    int y;
+};
+
+const int k_historyBufferSize = 4096;
+
 using ResizeCL = eventpp::CallbackList<void(int width, int height)>;
 
 class VerticalDisplay
@@ -22,12 +30,55 @@ private:
      *
      */
     DisplayBuffer<char> *m_charBuffer;
+
     /**
      * @brief The buffer that stores which position needs flush.
      * Clear all bits to 0 when flushing.
      *
      */
     DisplayBuffer<bool> *m_flushBitmap;
+
+    /**
+     * @brief The circular buffer to store history output.
+     *
+     */
+    char m_historyBuffer[k_historyBufferSize];
+
+    /**
+     * @brief The head buffer pointer.
+     *
+     */
+    char *m_bufferHead;
+
+    /**
+     * @brief The start position of console to display.
+     *
+     */
+    char *m_displayHead;
+
+    /**
+     * @brief The tail buffer pointer.
+     *
+     */
+    char *m_bufferTail;
+
+    /**
+     * @brief The number of lines in the history.
+     *
+     */
+    int m_historyLines;
+
+    /**
+     * @brief Displayed lines in the console.
+     *
+     */
+    int m_displayLines;
+
+    /**
+     * @brief The position of veirtual cursor.
+     *
+     */
+    CursorPos m_virtualCursorPos;
 
     /**
      * @brief The console resize event passed in to the constructor.
@@ -74,6 +125,13 @@ public:
      * @param val the char value.
      */
     void UpdateChar(int x, int y, char val);
+
+    /**
+     * @brief Push a string into the buffer.
+     *
+     * @param str the new string.
+     */
+    void PushString(std::string str);
 
     /**
      * @brief Get the Char Buffer directly.
